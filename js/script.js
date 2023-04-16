a4769322ae242cfc71e6f689dc70dea2040bec92
PaulAlvarez13 <101508702+PaulAlvarez13@users.noreply.github.com>
2023-04-16 17:13:15 +0800

    added class update completed

--- js/script.js
+++ js/script.js
@@ -12,5 +12,19 @@ function onAddTaskClicked(event) {
     var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
     todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
 }
+
+function onTodolistClicked(event) {
+    var targetElement = event.target;
+    while (!targetElement.classList.contains("task")) {
+        targetElement = targetElement.parentElement;
+    }
+    var checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked) {
+        targetElement.classList.add("completed");
+    } else {
+        targetElement.classList.remove("completed");
+    }
+
 // Step 3 Link to the Event Handler (link the behaviour)
-addTaskButton.addEventListener('click', onAddTaskedClicked);
+addTaskButton.addEventListener('click', onAddTaskClicked);
+todoListContainer.addEventListener('click', onTodolistClicked);
