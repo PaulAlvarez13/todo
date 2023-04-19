d14acea440a25cae08f053c75c6a827632f1f199
PaulAlvarez13 <101508702+PaulAlvarez13@users.noreply.github.com>
2023-04-19 23:15:42 +0800

    added permanent storage functionality

--- js/script.js
+++ js/script.js
@@ -8,12 +8,18 @@ var showCompletedButton = document.getElementById("show-completed")
 var templateElement = document.getElementById("list-item-template");
 var template = templateElement.innerHTML;
 
+function saveTasks(name, isCompleted) {
+    localStorage.setItem(name, isCompleted)
+}
+
 // Step 2 Write the behaviour
 function onAddTaskClicked(event) {
     var taskName = newTaskInput.value;
     newTaskInput.value = "";
     var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
     todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
+
+    saveTasks(taskName, false)
 }
 
 function showAllTasks() {
@@ -58,11 +64,31 @@ function onTodolistClicked(event) {
     } else {
         targetElement.classList.remove("completed");
     }
+
+    var taskNameElement = targetElement.querySelector(".task-name")
+    var taskName = taskNameElement.innerText;
+
+    saveTasks(taskName, checkbox.checked)
 }
 
+function renderTasks() {
+    for (i = 0; i< localStorage.length; i++){
+
+        var taskName = localStorage.key(i)
+        var isCompleted = localStorage.getItem(taskName) == "true";
+        var taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+        if (!isCompleted){
+            todoListContainer.insertAdjacentHTML('afterbegin', taskHTML)
+        }
+    }
+}
 // Step 3 Link to the event handler (link the behaviour)
 addTaskButton.addEventListener('click', onAddTaskClicked);
 todoListContainer.addEventListener('click', onTodolistClicked);
 showActiveButton.addEventListener('click', showActiveTasks);
 showAllButton.addEventListener('click', showAllTasks);
 showCompletedButton.addEventListener('click', showCompletedTasks);
+
+
+
+renderTasks();
